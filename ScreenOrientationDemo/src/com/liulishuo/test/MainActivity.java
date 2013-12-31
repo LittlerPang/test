aa9b27db128a8893f68cd7c61f7a7c42750c8d7a
Littler <768888479@qq.com>
2013-12-31 23:29:20 +0800

    测试一下！

---
+++ ScreenOrientationDemo/src/com/liulishuo/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.liulishuo.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.liulishuo.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
